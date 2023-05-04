65cb6b13b8f3587dba49ba634329bf483222f5e5
liguo <mdbsliguo@qq.com>
2023-05-04 22:41:23 +0800

    feat(按需导入elementplus): feat: commit与代码规范

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+// webpack.config.js
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  configureWebpack: (config) => {
+    config.plugins.push(
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      })
+    )
+    config.plugins.push(
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    )
+  }
+}
